2a18957fa193f7d5ab1e1c7f7e649bc16b33e11f
Zein Fikry <zenfikry@gmail.com>
2022-05-29 20:43:32 +0700

    add deposit system

--- CarRent.sol
+++ CarRent.sol
@@ -7,6 +7,7 @@ contract CarRent {
 
     uint public countCar = 0;
     uint public countRentAgreement = 0;
+    uint totalFund = 0;
 
     enum CarStatus {AVAILABLE, NOT_AVAILABLE}
     enum CarType {HATCHBACK, MPV, SUV}
@@ -21,6 +22,7 @@ contract CarRent {
         string carBrand;
         CarType carType;
         uint rentFee;
+        uint depositFee;
         uint penaltyFee;
         CarStatus carStatus;
         uint currentRentAgreementId;
@@ -31,15 +33,10 @@ contract CarRent {
     struct RentAgreement {
         uint rentAgreementId;
         uint carId;
-        string carName;
-        string carBrand;
-        uint rentFee;
-        uint penaltyFee;
         address payable carRenter;
         uint timestamp;
         uint rentPeriod;
         bool isGetPenalty;
-        bool isPenaltyPaid;
         bool isAgreementCompleted;
     }
     mapping(uint => RentAgreement) public RentAgreements;
@@ -54,84 +51,76 @@ contract CarRent {
         _;
     }
 
-    modifier isSameRenter(uint _carId) {
-        require(msg.sender == Cars[_carId].currentRenter, "You are not current renter");
-        _;
-    }
-
     modifier isCarAvailable(uint _carId) {
         require(Cars[_carId].carStatus == CarStatus.AVAILABLE, "The car is not available");
         _;
     }
 
     modifier rentFeeCheck(uint _carId, uint _rentPeriod) {
-        uint totalFee = Cars[_carId].rentFee * _rentPeriod * (1 ether);
-        require(msg.value >= totalFee, "Not enough Ether to pay rent fee");
+        uint totalFee = (Cars[_carId].depositFee + (Cars[_carId].rentFee * _rentPeriod)) * 1 ether;
+        require(msg.value == totalFee, "Your payment is not fit with rent fee");
         _;
     }
 
-    modifier penaltyCheck(uint _carId) {
-        uint currentRentAgreementId = Cars[_carId].currentRentAgreementId;
-        RentAgreement memory agreement = RentAgreements[currentRentAgreementId];
-        // uint dueDate = agreement.timestamp + (agreement.rentPeriod * 1 days);
-        uint dueDate = agreement.timestamp + (agreement.rentPeriod * 1 minutes);
-        require(block.timestamp <= dueDate || (agreement.isGetPenalty == true && agreement.isPenaltyPaid == true), "Your agreement is expired. Please pay for penalty");
-        _;
-    }
-
-    modifier isAgreementExpired(uint _carId) {
-        uint currentRentAgreementId = Cars[_carId].currentRentAgreementId;
-        RentAgreement memory agreement = RentAgreements[currentRentAgreementId];
-        // uint dueDate = agreement.timestamp + (agreement.rentPeriod * 1 days);
-        uint dueDate = agreement.timestamp + (agreement.rentPeriod * 1 minutes);
-        require(block.timestamp > dueDate, "You still have time for this car");
-        _;
-    }
-
-    function addCar(string memory _carName, string memory _carBrand, CarType _carType, uint _rentFee, uint _penaltyFee) external isOwner {
+    function addCar(string memory _carName, string memory _carBrand, CarType _carType, uint _rentFee, uint _depositFee, uint _penaltyFee) external isOwner {
         countCar++;
-        Cars[countCar] = Car(countCar, _carName, _carBrand, _carType, _rentFee, _penaltyFee, CarStatus.AVAILABLE, 0, address(0));
+        Cars[countCar] = Car(countCar, _carName, _carBrand, _carType, _rentFee, _depositFee, _penaltyFee, CarStatus.AVAILABLE, 0, address(0));
     }
 
     event eventRentCar(uint _carId, address _carRenter, uint _rentFee);
 
     function rentCar(uint _carId, uint _rentPeriod) external payable isNotOwner isCarAvailable(_carId) rentFeeCheck(_carId, _rentPeriod) {
-        // get car data
-        Car memory car = Cars[_carId];
-
         // create rent agreement
         countRentAgreement++;
-        RentAgreements[countRentAgreement] = RentAgreement(countRentAgreement, _carId, car.carName, car.carBrand, car.rentFee, car.penaltyFee, msg.sender, block.timestamp, _rentPeriod, false, false, false);
+        RentAgreements[countRentAgreement] = RentAgreement(countRentAgreement, _carId, msg.sender, block.timestamp, _rentPeriod, false, false);
 
         // update car data
         Cars[_carId].carStatus = CarStatus.NOT_AVAILABLE;
         Cars[_carId].currentRentAgreementId = countRentAgreement;
         Cars[_carId].currentRenter = msg.sender;
 
-        owner.transfer(msg.value);
+        totalFund += msg.value;
+
         emit eventRentCar(_carId, msg.sender, msg.value);
     }
 
-    function returnCar(uint _carId) external isSameRenter(_carId) penaltyCheck(_carId) {
-        uint currentRentAgreementId = Cars[_carId].currentRentAgreementId;
-        // set flag agreement status
+    event eventReturnCar(uint _carId, address _carRenter, bool isGetPenalty, uint returnDeposit);
+
+    function returnCar(uint _carId) external payable isOwner {
+        Car memory car = Cars[_carId];
+        uint currentRentAgreementId = car.currentRentAgreementId;
+        RentAgreement memory agreement = RentAgreements[currentRentAgreementId];
+
+        // return deposit
+        uint totalFee = agreement.rentPeriod * car.rentFee * 1 ether;
+        uint returnDeposit = car.depositFee * 1 ether;
+        // uint dueDate = agreement.timestamp + (agreement.rentPeriod * 1 days);
+        uint dueDate = agreement.timestamp + (agreement.rentPeriod * 1 minutes);
+        bool isGetPenalty = false;
+        if (block.timestamp > dueDate) {
+            isGetPenalty = true;
+            totalFee += car.penaltyFee * 1 ether;
+            returnDeposit -= car.penaltyFee * 1 ether;
+        }
+
+         // set flag agreement status
+        RentAgreements[currentRentAgreementId].isGetPenalty = isGetPenalty;
         RentAgreements[currentRentAgreementId].isAgreementCompleted = true;
 
         // reset car data
         Cars[_carId].carStatus = CarStatus.AVAILABLE;
         Cars[_carId].currentRentAgreementId = 0;
         Cars[_carId].currentRenter = address(0);
+
+        address payable renter = car.currentRenter;
+        renter.transfer(returnDeposit);
+        owner.transfer(totalFee);
+        totalFund -= (totalFee + returnDeposit);
+
+        emit eventReturnCar(_carId, renter, isGetPenalty, returnDeposit);
     }
 
-    function payPenalty(uint _carId) external payable isSameRenter(_carId) isAgreementExpired(_carId) {
-        uint currentRentAgreementId = Cars[_carId].currentRentAgreementId;
-
-        // set flag get penalty
-        RentAgreements[currentRentAgreementId].isGetPenalty = true;
-
-        uint penaltyFee = Cars[_carId].penaltyFee * (1 ether);
-        require(msg.value >= penaltyFee, "Not enough Ether to pay penalty");
-        owner.transfer(msg.value);
-        RentAgreements[currentRentAgreementId].isPenaltyPaid = true;
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
     }
 }
